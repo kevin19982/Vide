4d82d12430c2f244101fac47eac26785157432f7
Felix Froschauer <felix.froschauer@student.uni-tuebingen.de>
2022-11-14 19:46:26 +0100

    preprocessing of the raw data

--- data_extraction.R
+++ data_extraction.R
@@ -1,18 +1,14 @@
 # get all annual reports for all companies and years listed below
 
-# importing libraries
-library(httr)
-library(jsonlite)
-library(dplyr)
-library(readr)
-library(data.table)
-library(stringr)
-library(readr)
-library(xml2)
-library(rjson)
 
-# api package
-library(edgar)
+# importing libraries
+library(edgar) # API package
+library(jsonlite) # read_json
+library(data.table) # rbindlist()
+library(ff) # move order to different directory
+library(magrittr) # %>%
+library(tidyr) # only for the gather function => specific package?
+
 
 
 # set working directory
@@ -22,18 +18,35 @@ setwd("/Users/felix/Documents/git/Vide")
 # a list of CIK codes from the API 
 # for the API, company codes are deeded to get the reports
 # they can be gathered here:
-
-
 company_codes_all <- read_json("https://www.sec.gov/files/company_tickers.json") %>% rbindlist() # API request to get the company codes
+
+# save the file 
 write.csv(company_codes_all, "data/company_codes_all.csv", row.names = FALSE) # for late use
 
+
 # for now we get the codes manually
-
-
 #### data for our model
+
+### need to be set as variable (the dataframe!)
+
 company_names <- c("amazon", "walmart", "cocacola", "apple", "ebay")
 company_codes <- c(1018724, 104169, 21344, 320193, 1065088)
 relevant_years <- c(2020, 2021, 2022)
+
+year_2020 <- rep(2020, 5)
+year_2021 <- rep(2021, 5)
+year_2022 <- rep(2022, 5)
+
+
+data <- data.frame(name = company_names, code = company_codes, year_2020, year_2021, year_2022) %>% 
+  gather(key="delete", value="year", 3:5) %>% select(-delete)
+
+
+# export the data for later use
+write.csv(data, "data/our_company_list.csv", row.names = FALSE)
+
+
+
 
 
 # define the user agent (needed for an APi request here)
@@ -49,202 +62,13 @@ get_fillings <- getFilingsHTML(company_codes, "10-K", relevant_years, quarter = 
 # we can delete folder full_text and folder Master Indexes ()
 
 
+#### move the data into the data directory
 
+old_path <- "Edgar filings_HTML view"
+new_path <- "data/all_fillings_html"
 
-# 
-# html_file <- read_html("Edgar filings_HTML view/Form 10-K/21344/21344_10-K_2020-02-24_0000021344-20-000006.html")
-# text_file <- html_text(html_file)
-# 
-# text_file
-# 
-# write_file(text_file, "new_file_report.txt")
+file.move(old_path, new_path)
 
 
 
-# now store the data in the right folder and make the format more intuitive
-# path <- paste0("Edgar filings_HTML view/Form 10-K/", company_codes[1], "/")
-# 
-# 
-# 
-# old_name <- list.files(path)[1]
-# new_name <- paste0(path, company_names[1], "_", relevant_years[1], ".html")
-# 
-# file.rename(paste0(path, old_name), paste0(path, new_name))
-# 
-
-
-
-
-
-
-
-
-
-
-
-# variables
-# 
-# 
-# list_of_companies <- c("amazon")
-# 
-# ### working with edgar package
-# 
-# 
-# get_fillings <- getFilingsHTML(1018724, "10-K", 2020, quarter = 1, useragent = user_agent)
-# 
-# amazon <- 1018724
-# walmart <- 104169
-# cola <- 21344
-# airbnb <- 1559720
-# 
-# 
-# 
-# 
-# 
-# get_fillings <- getFilingsHTML(airbnb, "10-K", 2021, quarter = 1, useragent = user_agent)
-# # transform data into minimal HTML? to make it readable for the model
-# 
-# result <- read_html("Edgar filings_HTML view/Form 10-K/21344/21344_10-K_2020-02-24_0000021344-20-000006.html")
-# 
-# 
-# data <- read_file("Edgar filings_full text/Form 10-K/21344/21344_10-K_2020-02-24_0000021344-20-000006.txt")
-# 
-# data
-# 
-# library(rvest)
-# 
-# 
-# strip_html <- function(s) {
-#   html_text(read_html(s))
-# }
-# 
-# 
-# html_file <- read_html("Edgar filings_HTML view/Form 10-K/21344/21344_10-K_2020-02-24_0000021344-20-000006.html")
-# 
-# text_file <- html_text(html_file)
-# 
-# text_file
-# 
-# write_file(text_file, "new_file_report.txt")
-# 
-
-
-
-
-##################### junk ###############################
-
-################## Part I: Get adjusted company codes ################## 
-
-# # get a list of CIK codes from the API 
-# cik_codes <- read_json("https://www.sec.gov/files/company_tickers.json")
-# cik_codes <- rbindlist(cik_codes)
-# write.csv(cik_codes, "data/company_tickers.csv", row.names = FALSE)
-# 
-# 
-# # to get the complete CIK codes, zeros have to be added such that to length is equal to 10
-# adjusted_cik_vector <- matrix(nrow = nrow(cik_codes), ncol = 1)
-# 
-# # this function is only called once in this script
-# adjust_cik_codes <- function(cik_codes) {
-#   for (i in 1:nrow(cik_codes)){
-#     
-#     #determine number of zeros needed
-#     num_zeros <- 10 - str_length(cik_codes$cik_str[i])
-#     list_zeros <- rep(0, num_zeros)
-#     string_zeros <- str_replace_all(str_replace_all(toString(list_zeros), ",", ""), " ", "")
-#     
-#     # add zeros to the beginning
-#     new_cik_code <- paste0(string_zeros, cik_codes$cik_str[i])
-#     
-#     # store the adjusted values in a predefined list
-#     adjusted_cik_vector[i] <- new_cik_code
-#   }
-#   
-#   # either return the vector or we could already change the column inside this function
-#   return(adjusted_cik_vector)
-# }
-# 
-# # execute function from above
-# new_column <- adjust_cik_codes(cik_codes)
-# cik_codes$cik_str <- new_column
-# 
-# 
-# write.csv(cik_codes, "data/company_tickers_adjusted.csv", row.names = FALSE)
-# 
-
-################## Part II: --------------------------------- ##################
-
-# # read in the data from above
-# adjusted_cik_codes <- read_csv("data/company_tickers_adjusted.csv")
-# 
-# 
-# 
-# # define url 
-# amazon_url <- "https://data.sec.gov/api/xbrl/companyfacts/CIK0001018724.json"
-# 
-# # GET to receive the data from the API (with user agent as needed https://www.sec.gov/os/accessing-edgar-data)
-# 
-# # get all data for amazon
-# amazon <- GET(url = amazon_url, config = add_headers(`User-Agent` = user_agent, 
-#                                                      `Accept-Encoding` = "gzip, deflate"))
-# amazon$status_code
-# 
-# content_2 <- content(amazon, as="text", encoding = "UTF-8") %>% fromJSON()
-# 
-# 
-# 
-# 
-# # get only specific data for amazon
-# 
-# specific <- "data.sec.gov/api/xbrl/frames/us-gaap/AccountsPayableCurrent/USD/CY2020.json"
-# 
-# specific_json <- GET(url = specific, config = add_headers(`User-Agent` = user_agent,
-#                                                           `Accept-Encoding` = "gzip, deflate"))
-# 
-# 
-# 
-# specific_json$status_code
-# b
-# 
-# raw <- content(specific_json, as="text", encoding = "UTF-8") %>% fromJSON(flatten=FALSE)
-# 
-# 
-# 
-# ## testing json file 
-# 
-# one_company <- fromJSON(file= "data/CIK0000001750.json")
-# 
-# print(one_company)
-# 
-# write.table(one_company, file = "data/one_company.txt") #, sep="\t", row.names = TRUE, col.names= NA)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#next_step <- amazon$content %>% read_html(encoding = "UTF-8", as="text") %>% fromJSON(amazon, flatten=FALSE)
-#data_raw <- try(content(amazon, as="text", encoding ="UTF-8") %>% fromJSON(amazon, flatten=FALSE))
-
-#amazon_2
-
-# amazon <- read_json(amazon_url, 
-#                     config = add_headers(
-#                       "User-Agent" = user_agent, 
-#                       "Accept-Encoding" = "gzip", "deflate"))$sz
-
-
-
-#object.size(year.master)/1000000
 
--- data_preprocessing.R
+++ data_preprocessing.R
@@ -7,39 +7,30 @@ library(dplyr)
 library(rvest)
 library(magrittr)
 
-setwd("~/R/DS 10_k report")
- 
 
-# read the html file 
-html_file <- read_html("Edgar filings_HTML view/Form 10-K/104169/104169_10-K_2020-03-20_0000104169-20-000011.html") 
-
-# select all tables in there
-html_tables_all <- html_table(html_file, fill = TRUE)
+setwd("/Users/felix/Documents/git/Vide")
 
 
-# 
-# # select a single table and store it in a dataframe
-# table_one <- html_tables_all[[101]] %>% as.data.frame()
-# 
-# # print the raw table
-# table_one
-# 
-# 
+########### Read in the HTML files and store them in a text format in the data/raw folder ########### 
 
 
-# write the template function
+# import function used in the loop
 table_to_text <- function(table) {
+
   
   # change empty values to NA
   table[table == ""] <- NA 
-  
   # delete columns with more than 10% NA
   table_new = table[,!sapply(table, function(x) mean(is.na(x)))> 0.9]
-  print(table_new)
+  
+  if (ncol (table_new) == 0){
+    empty_string = " "
+    return(empty_string)
+  } 
+  
+  
   # decide for header row (check if first entry of the row is not NA)
-  if (!is.na(table_new[1, 1])){
-    header_index <- 0
-  } else if(!is.na(table_new)[2, 1]){
+  if (!is.na(table_new[2, 1])){
     header_index <- 1
   } else if (!is.na(table_new)[3, 1]){
     header_index <- 2
@@ -51,15 +42,16 @@ table_to_text <- function(table) {
     header_index <- 5
   } else {header_index <- 5}
   
-
+ 
+  
   # header row stays the same in all iterations
   header = table_new[header_index, ]
-
+  
   start_row = header_index + 1
   
   #current_row = table_new[7, ]
   #sentence = " "
-
+  
   number_of_sentences <- nrow(table_new) - header_index
   
   # final output variable (appended over all rows and all columns)
@@ -68,7 +60,6 @@ table_to_text <- function(table) {
   
   # loop over all rows that are non header
   for (n in 1:number_of_sentences){
-    
     # reset sentence variable for each new row
     sentence <- " "
     current_row = table_new[header_index+n, ] #!!!!!!!!!!!!!!!!!!!
@@ -79,86 +70,425 @@ table_to_text <- function(table) {
       sentence <- paste0(sentence, text)
     }
     
-    text_file <- paste0(text_file, " \n ## ", sentence ) # why \t or \n not working?
+    text_file <- paste0(text_file, " \n ## ", sentence ) 
     
   }
-
-
   
-
   return(text_file)
   
 }
 
 
-# loop over all tables and append the texts into one big file 
+# import company list
+company_list <- read_csv("data/our_company_list.csv")
 
-final_text <- ""
 
-# go through all tables and get the text
-for (index in 1:length(html_tables_all)){
+
+# indexes
+# for each iteration, select year and company like this: company_list[i, 2] and company_list[i, 3]
+
+#loop_year <- company_list[1, 3]
+#loop_company <- company_list[1, 2]
+#nrow(company_list)
+
+# read the html file 
+html_path <- "data/all_fillings_html/Form 10-K/"
+counter <- 0
+year_index <- 0
+
+
+for (idx in 1:nrow(company_list)){
+  counter <- counter + 1
+  print(paste0(counter, "/", nrow(company_list)))
+
+
+  # get the yearly index for reports of a given company 
+  if (company_list[idx, 3] == 2022){
+    year_index <- 3
+  } else if (company_list[idx, 3] == 2021){
+ 
+    year_index <- 2
+  } else {
+    year_index <- 1
+
+  }
   
   
-  current_table <- html_tables_all[[index]] 
-  current_table_text <- table_to_text(current_table)
+  company <- company_list[idx, 2]
+  year <- company_list[idx, 3]
   
-  final_text <- paste0(final_text, " \n next table:", index, " ", current_table_text)
+  # get the full path of each report
+  company_path <- paste0(html_path, company)
+  file_name <- list.files(company_path)[year_index]
+  complete_path <- paste0(html_path, company, "/", file_name)
   
   
-  print(index)
+  ##########################################################################################
+  
+  # we need the file two times
+  # once for the normal written text and once to get all tables
+  html_file <- read_html(complete_path)
+  
+  ### the normal written text
+  html_normal_text <- html_text(html_file)
+  
+  ### extract all tables from the report
+  html_tables_all <- html_table(html_file, fill = TRUE)
+  
+  
+  html_table_text <- ""
+  for (index in 1:length(html_tables_all)){
+    
+
+    # selec a different table in each iteration
+    current_table <- html_tables_all[[index]] 
+    
+    current_table_text <- table_to_text(current_table)
+    html_table_text <- paste0(html_table_text, " \n next table:", index, " ", current_table_text)
+    
+    
+  }
+  
+  
+  
+  complete_report <- paste0(html_normal_text, "The table text starts here: ", html_table_text)
+  
+  
+  file_name <- paste0("data/annual_report_", company, "_", year, ".txt")
+  
+  write_file(complete_report, file_name)
+  
+  
 }
 
-final_text
-
-write_file(final_text, "final_table_data.txt")
 
 
 
-
+company <- company_list[1, 2]
+company
 
 
 
 
-##### junk 
 
-# change empty values to NA
+#####################JUNK####################################
+# # loop over annual report files
+# for (idx in 1:nrow(company_codes)){
+#   print(company_names[idx, 1])
+#   print("first loop")
+#   # get the correct path for each company
+#   company_path <- paste0(html_path, company_codes[idx, 1])
+#   
+#   
+#   for (year in 1:length(relevant_years)){
+#     print(relevant_years[year])
+#     print("second loop")
+#     counter <- counter + 1
+#     print(counter)
+#     # get the file name of the report
+#     file_name <- list.files(company_path)[year]
+#     
+#     # correct and final path of the report
+#     complete_path <- paste0(company_path, "/", file_name)
+#     
+#     
+#     ##########################################################################################
+# 
+#     # we need the file two times
+#     # once for the normal written text and once to get all tables
+#     html_file <- read_html(complete_path)
+#     
+#     ### the normal written text
+#     html_normal_text <- html_text(html_file)
+#     
+#     ### the tables 
+#     html_tables_all <- html_table(html_file, fill = TRUE)
+#     
+#     
+#     html_table_text <- ""
+#     
+#     # go through all tables and get the text
+#     for (index in 1:length(html_tables_all)){
+#       
+#       print("third loop")
+#       current_table <- html_tables_all[[index]] 
+#       print("after tables")
+#       current_table_text <- table_to_text(current_table)
+#       print("after function")
+#       html_table_text <- paste0(html_table_text, " \n next table:", index, " ", current_table_text)
+#       
+#       
+#       print(index)
+#     }
+#     
+#     
+#     
+#     complete_report <- paste0(html_normal_text, "The table text starts here: ", html_table_text)
+#     
+#   
+#     file_name <- paste0("data/annual_report_", company_names[idx, 1], "_", relevant_years[year], ".txt")
+#     
+#     write_file(complete_report, file_name)
+#     
+#     
+#   }
+#   
+# }
 
-# table_two <- table_one
+
+
+
+#####################older junk ###################################
+
+# 
+# file_name <- paste0("annual_report_", company_names[1, 1], "_", relevant_years[1], ".txt")
+# file_name
 # 
 # 
-# table_two[table_two == ""] <- NA
-# table_two = table_two[,!sapply(table_two, function(x) mean(is.na(x)))> 0.9]
+# testing
+# 
+# path <- "data/all_fillings_html/Form 10-K/21344/21344_10-K_2020-02-24_0000021344-20-000006.html"
+# html_file <- read_html(path)
 # 
 # 
-# if (!is.na(table_two[1, 1])){
-#   variable <- 0
-# } else if(!is.na(table_two)[2, 1]){
-#   variable <- 1
-# } else if (!is.na(table_two)[3, 1]){
-#   variable <- 2
-# } else if (!is.na(table_two)[4, 1]){
-#   variable <- 3
-# } else if (!is.na(table_two)[1, 1]){
-# 
-# } else {variable <- 999}
-# 
-# variable
-# 
-# header <- table_two[variable, ]
-# header
+# ### the tables
+# html_tables_all <- html_table(html_file, fill = TRUE)
 # 
 # 
-# start_row = variable + 1
-# start_row 
+# table <- html_tables_all[[1]]
 # 
-# number_of_sentences <- nrow(table_two) - variable
-# number_of_sentences
 # 
-# text_file <- " "
-# sentence <- " "
-# current_row <- table_two[variable+1,]
-# current_row
 # 
-# header[1]
+# next_one <- table_to_text(html_tables_all[[1]])
 # 
-# paste0(header[1], " ", current_row[1], " ")
+# 
+# next_one
+# 
+# 
+# 
+# # change empty values to NA
+# 
+# table[table == ""] <- NA 
+# 
+# print("function 1")
+# # delete columns with more than 10% NA
+# table_new = table[,!sapply(table, function(x) mean(is.na(x)))> 0.9]
+# 
+# print("function")
+# print(table_new)
+# # decide for header row (check if first entry of the row is not NA)
+# 
+# if (!is.na(table_new[1, 1])){
+#   header_index <- 0
+# } else if(!is.na(table_new)[2, 1]){
+#   header_index <- 1
+# } else if (!is.na(table_new)[3, 1]){
+#   header_index <- 2
+# } else if (!is.na(table_new)[4, 1]){
+#   header_index <- 3
+# } else if (!is.na(table_new)[5, 1]){
+#   header_index <- 4
+# } else if (!is.na(table_new)[6, 1]){
+#   header_index <- 5
+# } else {header_index <- 5}
+# 
+# 
+# 
+# 
+# header_index
+# 
+# 
+
+
+#html_file <- read_html("Edgar filings_HTML view/Form 10-K/104169/104169_10-K_2020-03-20_0000104169-20-000011.html") 
+
+
+# 
+# html_file <- read_html("Edgar filings_HTML view/Form 10-K/21344/21344_10-K_2020-02-24_0000021344-20-000006.html")
+# text_file <- html_text(html_file)
+# 
+# text_file
+# 
+# write_file(text_file, "new_file_report.txt")
+
+
+
+
+
+
+
+
+
+
+
+
+
+########### Read in the HTML files and transform the tables into strings and add them ########### 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 
+# 
+# 
+# 
+# # select all tables in there
+# html_tables_all <- html_table(html_file, fill = TRUE)
+# 
+# 
+# # 
+# # # select a single table and store it in a dataframe
+# # table_one <- html_tables_all[[101]] %>% as.data.frame()
+# # 
+# # # print the raw table
+# # table_one
+# # 
+# # 
+# 
+# 
+# # write the template function
+# table_to_text <- function(table) {
+#   
+#   # change empty values to NA
+#   table[table == ""] <- NA 
+#   
+#   # delete columns with more than 10% NA
+#   table_new = table[,!sapply(table, function(x) mean(is.na(x)))> 0.9]
+#   print(table_new)
+#   # decide for header row (check if first entry of the row is not NA)
+#   if (!is.na(table_new[1, 1])){
+#     header_index <- 0
+#   } else if(!is.na(table_new)[2, 1]){
+#     header_index <- 1
+#   } else if (!is.na(table_new)[3, 1]){
+#     header_index <- 2
+#   } else if (!is.na(table_new)[4, 1]){
+#     header_index <- 3
+#   } else if (!is.na(table_new)[5, 1]){
+#     header_index <- 4
+#   } else if (!is.na(table_new)[6, 1]){
+#     header_index <- 5
+#   } else {header_index <- 5}
+#   
+# 
+#   # header row stays the same in all iterations
+#   header = table_new[header_index, ]
+# 
+#   start_row = header_index + 1
+#   
+#   #current_row = table_new[7, ]
+#   #sentence = " "
+# 
+#   number_of_sentences <- nrow(table_new) - header_index
+#   
+#   # final output variable (appended over all rows and all columns)
+#   text_file <- " "
+#   
+#   
+#   # loop over all rows that are non header
+#   for (n in 1:number_of_sentences){
+#     
+#     # reset sentence variable for each new row
+#     sentence <- " "
+#     current_row = table_new[header_index+n, ] #!!!!!!!!!!!!!!!!!!!
+#     
+#     # loop in a certain row over all columns
+#     for (i in 1:ncol(table_new)){
+#       text <- paste0(header[i], " ", current_row[i], " ")
+#       sentence <- paste0(sentence, text)
+#     }
+#     
+#     text_file <- paste0(text_file, " \n ## ", sentence ) # why \t or \n not working?
+#     
+#   }
+# 
+# 
+#   
+# 
+#   return(text_file)
+#   
+# }
+# 
+# 
+# # loop over all tables and append the texts into one big file 
+# 
+# final_text <- ""
+# 
+# # go through all tables and get the text
+# for (index in 1:length(html_tables_all)){
+#   
+#   
+#   current_table <- html_tables_all[[index]] 
+#   current_table_text <- table_to_text(current_table)
+#   
+#   final_text <- paste0(final_text, " \n next table:", index, " ", current_table_text)
+#   
+#   
+#   print(index)
+# }
+# 
+# final_text
+# 
+# write_file(final_text, "final_table_data.txt")
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# ##### oldest junk ############# 
+# 
+# # change empty values to NA
+# 
+# # table_two <- table_one
+# # 
+# # 
+# # table_two[table_two == ""] <- NA
+# # table_two = table_two[,!sapply(table_two, function(x) mean(is.na(x)))> 0.9]
+# # 
+# # 
+# # if (!is.na(table_two[1, 1])){
+# #   variable <- 0
+# # } else if(!is.na(table_two)[2, 1]){
+# #   variable <- 1
+# # } else if (!is.na(table_two)[3, 1]){
+# #   variable <- 2
+# # } else if (!is.na(table_two)[4, 1]){
+# #   variable <- 3
+# # } else if (!is.na(table_two)[1, 1]){
+# # 
+# # } else {variable <- 999}
+# # 
+# # variable
+# # 
+# # header <- table_two[variable, ]
+# # header
+# # 
+# # 
+# # start_row = variable + 1
+# # start_row 
+# # 
+# # number_of_sentences <- nrow(table_two) - variable
+# # number_of_sentences
+# # 
+# # text_file <- " "
+# # sentence <- " "
+# # current_row <- table_two[variable+1,]
+# # current_row
+# # 
+# # header[1]
+# # 
+# # paste0(header[1], " ", current_row[1], " ")
