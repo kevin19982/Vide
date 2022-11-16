6e44d306b6a9abce48045e80ba21e1b2ff5785e7
kevin19982 <83638356+kevin19982@users.noreply.github.com>
2022-11-16 17:00:07 +0100

    trying to tackle the memory issues on streamlit

--- main_page.py
+++ main_page.py
@@ -13,6 +13,7 @@ query = st.text_input("Mortal, pose your request!")
 from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 from pathlib import Path
 
+@st.cache
 file_10k = open("new_file_test.txt", encoding = "utf8")
 text_10k = file_10k.read()
 #text_10k = "Trees tend to overfit if the gardener ceases to stop their growth to achieve greatness."
