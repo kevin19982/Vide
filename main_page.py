9b32b9c9ff1147e4d8a00fdb51514d262fd5e885
kevin19982 <83638356+kevin19982@users.noreply.github.com>
2022-11-16 23:01:37 +0100

    trying to get the website back up

--- main_page.py
+++ main_page.py
@@ -13,9 +13,9 @@ query = st.text_input("Mortal, pose your request!")
 from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 from pathlib import Path
 
-file_10k = open("new_file_test.txt", encoding = "utf8")
-text_10k = file_10k.read()
-#text_10k = "Trees tend to overfit if the gardener ceases to stop their growth to achieve greatness."
+#file_10k = open("new_file_test.txt", encoding = "utf8")
+#text_10k = file_10k.read()
+text_10k = "Trees tend to overfit if the gardener ceases to stop their growth to achieve greatness."
 
 model_name = "deepset/roberta-base-squad2"
 
