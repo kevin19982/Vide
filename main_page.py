24a37e549824cf93c694f8f9ad31712cf4cce814
kevin19982 <83638356+kevin19982@users.noreply.github.com>
2022-11-22 21:46:30 +0100

    test updated website with retriever model

--- main_page.py
+++ main_page.py
@@ -1,28 +1,77 @@
 # pages
 
-# load packages
+# load streamlit package
 import streamlit as st
 
 st.markdown("# Main page 🎈")
 st.sidebar.markdown("# Main page 🎈")
 
+# upload file
+file_10k = st.file_uploader("Please upload a txt of the document you would like to examine.")
 # input user query
-query = st.text_input("Mortal, pose your request!")
+query = st.text_input("Mortal being, pose your request!")
 
+# retriever
 # import packages
+import nltk  # natural language toolkit
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize, wordpunct_tokenize
+import glob
+import re
+import os
+import numpy as np
+import sys
+import math
+import operator
+from sentence_transformers import SentenceTransformer
+import sentence_transformers
+from io import StringIO
+nltk.download("stopwords")
+nltk.download("punkt")
+nltk.download("averaged_perceptron_tagger")
+nltk.download("wordnet")
+nltk.download('omw-1.4')
+
+# load model
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+# text is english
+Stopwords = set(stopwords.words("english")) 
+
+# transform document
+file = StringIO(file_10k.getvalue().decode("utf-8"))
+text = file.read()
+
+# split text into sentences
+text_sentences = text.split(". ")
+
+# calculate embeddings
+embeddings_query = model.encode(query, convert_to_tensor = True)
+embeddings_sentences = model.encode(text_sentences, convert_to_tensor = True)  # the longest step
+
+# compute cosine-similarities
+cosine_scores_query = sentence_transformers.util.pytorch_cos_sim(embeddings_query, embeddings_sentences)
+
+# get indices of highest cosine scores
+idx = np.where(cosine_scores_query[0] > 0.4)[0]
+
+# join retrieved sentences
+sentences_answer = ". ".join(np.array(text_sentences)[idx])
+
+
+# q and a
+# loading packages
 from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 from pathlib import Path
 
-#file_10k = open("new_file_test.txt", encoding = "utf8")
-#text_10k = file_10k.read()
-text_10k = "Trees tend to overfit if the gardener ceases to stop their growth to achieve greatness."
-
+# get an answer to the question
 model_name = "deepset/roberta-base-squad2"
 
 nlp = pipeline("question-answering", model = model_name, tokenizer = model_name)
 QA_input = {
     "question": query,
-    "context": text_10k
+    "context": sentences_answer
 }
 
 res = nlp(QA_input)
@@ -30,4 +79,5 @@ res = nlp(QA_input)
 model = AutoModelForQuestionAnswering.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-st.write("result:", res)
+st.write("Answer:", res)
+
