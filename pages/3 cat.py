cac868caa4f3aac6eb7cac847e61f5f5cd4e1aa8
kevin19982 <83638356+kevin19982@users.noreply.github.com>
2022-12-07 23:21:32 +0100

    adjusted file for github

--- pages/3 cat.py
+++ pages/3 cat.py
@@ -6,5 +6,5 @@ st.markdown("# Page 3 🎉")
 st.sidebar.markdown("# Page 3 🎉")
 
 from PIL import Image
-im = Image.open("C:/Users/siriv/Downloads/Data_Science_Project/scripts/examples/hello.jpg")
+im = Image.open("hello.jpg")
 st.image(im)
