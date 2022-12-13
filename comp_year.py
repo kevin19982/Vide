fd407c03a752a6556308a8fc4ed26940da4e6924
kevin19982 <83638356+kevin19982@users.noreply.github.com>
2022-12-13 22:21:49 +0100

    add file for new function of main script

---
+++ comp_year.py
@@ -0,0 +1,7 @@
+# companies and years
+
+# import packages
+import pandas as pd
+
+# dataframe for company-year-combinations
+comp_year_df = pd.read_csv("C:/Users/siriv/Downloads/Data_Science_Project/scripts/data/company_year.csv")
