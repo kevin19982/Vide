2efcae259122cf054537f1d907793300a8369f60
Felix Froschauer <felix.froschauer@student.uni-tuebingen.de>
2023-02-16 18:12:35 +0100

    new data files

--- data_generating.R
+++ data_generating.R
@@ -12,7 +12,8 @@ library(tidyr) # only for the gather function => specific package?
 
 
 # set working directory
-setwd("/Users/felix/Documents/git/Vide/new/recent_12_02/Vide")
+#setwd("/Users/felix/Documents/git/Vide/new/recent_12_02/Vide")
+setwd("/Users/felix/Documents/cloud/Data-testing")
 
 
 # get a list of company codes from the API 
@@ -26,8 +27,9 @@ write.csv(company_codes_all, "data/company_codes_all.csv", row.names = FALSE) # 
 #### data for our model
 
 company_names <- c("amazon", "walmart", "cocacola", "apple", "ebay")
-company_codes <- c(1018724, 104169, 21344, 320193, 1065088)
-relevant_years <- c(2020, 2021, 2022)
+#company_codes <- c(1018724, 104169, 21344, 320193, 1065088)
+company_codes <- c(1018724, 1065088, 21344)
+relevant_years <- c(2021, 2022)
 
 
 
@@ -43,10 +45,11 @@ user_agent <- "Felix Froschauer felix.froschauer@student.uni-tuebingen.de"
 
 # getFilingsHTML retrieves all specified company-year 10-k reports as .txt and .html
 get_fillings <- getFilingsHTML(company_codes, "10-K", relevant_years, quarter = 1, useragent = user_agent)
+
 write.csv(get_fillings, "data/our_company_list.csv", row.names = FALSE)
 
 
-# for a cleaner structure, we can delete folder "full_text" and folder "Master Indexes"
+# for a clean structure, we can delete folder "full_text" and folder "Master Indexes"
 # and move the relevant under a more obvious structure
 
 
@@ -62,4 +65,9 @@ new_path_text <- "data/all_filings_txt"
 
 file.move(old_path_text, new_path_text)
   
+# not needed, can be deleted
+old_path_idx <- "Master Indexes"
+new_path_idx <- "data/master_indexes"
+
+file.move(old_path_idx, new_path_idx)
 
